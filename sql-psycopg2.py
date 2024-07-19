5bfdbec754ecfd541b543f99606201ca89510e5f
Craig Allen <callen7@hotmail.co.uk>
2024-07-19 06:51:59 +0000

    added python files

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -26,10 +26,10 @@ cursor = connection.cursor()
 cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
 
 # fetch the results (multiple)
-# results = cursor.fetchall()
+results = cursor.fetchall()
 
 # fetch the result (single)
-results = cursor.fetchone()
+# results = cursor.fetchone()
 
 # close the connection
 connection.close()
